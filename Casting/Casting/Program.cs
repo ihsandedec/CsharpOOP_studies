69a0fab48f18481ba08d948687abdc0551387b9b
ihsandedec <ddcihsan@gmail.com>
2021-08-11 00:18:48 +0300

    Casting çalışması eklendi

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 9;
+            short y = 14;
+            int z = 2;
+            long t = 4;
+            float k = 21;
+
+            /* z = y; hata alırız çünkü küçük olan büyük olana atanır. İmplicit Casting  */
+            y = x;
+            z = y;
+            k = x;
+
+            string f;
+
+            /* f = x; Hata alırız çünkü değişken tipleri farklıdır Explicit Casting olarak yapmamız gerek */
+
+            double a = 13;
+            int b;
+            b = Convert.ToInt32(x); // yada y = (int)x olarakda yazabiliiz.
+            string h = a.ToString();
+
+
+            //Değişkenler en son aldığı değeri tutar.
+ 
+
+        }
+    }
+}
