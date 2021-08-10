ef9d1e180f11ab501226cb0a0a4649568e0447c5
ihsandedec <ddcihsan@gmail.com>
2021-08-11 00:15:24 +0300

    WriteLine ReadLine çalışması eklendi

---
+++ WriteLine-Readlıne/WriteLine-Readlıne/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WriteLine_Readlıne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name :"); //ekrana yazdırma
+            string name = Console.ReadLine();    // Kullanıcıdan bilgi isteme
+
+            Console.Write("What is your surname :");
+            string surname = Console.ReadLine();
+
+            Console.Write("What is your age :");
+            string age = Console.ReadLine();
+            Console.WriteLine("Welcome to " + name + " " + surname + " your age is "+ age);
+
+            /*
+            Console.Write("Enter your age :");
+            int age = Console.Read();    // Kullanıcıdan bilgi isteme ascii kodunu bize verir
+            Console.WriteLine("Your Age " + age);
+            Console.ReadKey();
+            */
+        }
+    }
+}
