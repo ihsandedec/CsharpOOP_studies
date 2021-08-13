c6375208ac4ed8c001a4d23d952a8ac7299115b6
ihsandedec <ddcihsan@gmail.com>
2021-08-13 13:48:43 +0300

    If_Else_Ternary çalışması eklendi

---
+++ if_else/if_else/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace if_else
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnmessage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnternary = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnmessage
+            // 
+            this.btnmessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnmessage.Location = new System.Drawing.Point(461, 43);
+            this.btnmessage.Name = "btnmessage";
+            this.btnmessage.Size = new System.Drawing.Size(118, 46);
+            this.btnmessage.TabIndex = 0;
+            this.btnmessage.Text = "Message";
+            this.btnmessage.UseVisualStyleBackColor = true;
+            this.btnmessage.Click += new System.EventHandler(this.btnmessage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnternary
+            // 
+            this.btnternary.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnternary.Location = new System.Drawing.Point(461, 209);
+            this.btnternary.Name = "btnternary";
+            this.btnternary.Size = new System.Drawing.Size(118, 46);
+            this.btnternary.TabIndex = 2;
+            this.btnternary.Text = "Ternary";
+            this.btnternary.UseVisualStyleBackColor = true;
+            this.btnternary.Click += new System.EventHandler(this.btnternary_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(13, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(409, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 348);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnternary);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnmessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnmessage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnternary;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ if_else/if_else/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_else
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmessage_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bu Ürünü Almak İster Misiniz ?", "Control", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            if (result == DialogResult.Yes)
+            {
+                textBox1.Text = "Bizi Tercih Ettiğiniz İçin Teşekkürler ";
+            }
+            else if (result == DialogResult.No)
+            {
+                textBox1.Text = "Yinede Teşekkür Ederiz";
+            }
+            else
+            {
+                textBox1.Text = "Çıkış Yapmadan Mutlaka Diğer Ürünlerimeze de Bakınız";
+            }
+        }
+
+        private void btnternary_Click(object sender, EventArgs e)
+        {
+            //soru işaretinden önceki değer kontrol edilen değişken , doğru ise : solundaki değer çalışır (if) , yanlışsa sağındaki değer çalışır (else)
+            int a = 5, b = 10;
+            string ternaryresult;
+            ternaryresult = a < b ? b.ToString() : a.ToString();
+            textBox2.Text = ternaryresult;
+        }
+    }
+}
