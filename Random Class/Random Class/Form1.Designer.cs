f78878b7333c660f99bef8bfb2fe2c1c028923ba
ihsandedec <ddcihsan@gmail.com>
2021-08-24 18:20:12 +0300

    Random class çalışması eklendi

---
+++ Random Class/Random Class/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Random_Class
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRandom01 = new System.Windows.Forms.Button();
+            this.btnRandom02 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRandom03 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRandom04 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnRandom05 = new System.Windows.Forms.Button();
+            this.btnRandom06 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btnRandom01
+            // 
+            this.btnRandom01.Location = new System.Drawing.Point(228, 30);
+            this.btnRandom01.Name = "btnRandom01";
+            this.btnRandom01.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom01.TabIndex = 1;
+            this.btnRandom01.Text = "Random01";
+            this.btnRandom01.UseVisualStyleBackColor = true;
+            this.btnRandom01.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnRandom02
+            // 
+            this.btnRandom02.Location = new System.Drawing.Point(228, 74);
+            this.btnRandom02.Name = "btnRandom02";
+            this.btnRandom02.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom02.TabIndex = 3;
+            this.btnRandom02.Text = "Random02";
+            this.btnRandom02.UseVisualStyleBackColor = true;
+            this.btnRandom02.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // btnRandom03
+            // 
+            this.btnRandom03.Location = new System.Drawing.Point(228, 128);
+            this.btnRandom03.Name = "btnRandom03";
+            this.btnRandom03.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom03.TabIndex = 5;
+            this.btnRandom03.Text = "Random03";
+            this.btnRandom03.UseVisualStyleBackColor = true;
+            this.btnRandom03.Click += new System.EventHandler(this.btnRandom03_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // btnRandom04
+            // 
+            this.btnRandom04.Location = new System.Drawing.Point(228, 181);
+            this.btnRandom04.Name = "btnRandom04";
+            this.btnRandom04.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom04.TabIndex = 6;
+            this.btnRandom04.Text = "Random04";
+            this.btnRandom04.UseVisualStyleBackColor = true;
+            this.btnRandom04.Click += new System.EventHandler(this.btnRandom04_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(365, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(424, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(505, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(568, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(365, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(424, 239);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(505, 239);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(568, 239);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "label11";
+            // 
+            // btnRandom05
+            // 
+            this.btnRandom05.Location = new System.Drawing.Point(228, 234);
+            this.btnRandom05.Name = "btnRandom05";
+            this.btnRandom05.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom05.TabIndex = 11;
+            this.btnRandom05.Text = "Random05";
+            this.btnRandom05.UseVisualStyleBackColor = true;
+            this.btnRandom05.Click += new System.EventHandler(this.btnRandom05_Click);
+            // 
+            // btnRandom06
+            // 
+            this.btnRandom06.Location = new System.Drawing.Point(228, 291);
+            this.btnRandom06.Name = "btnRandom06";
+            this.btnRandom06.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom06.TabIndex = 16;
+            this.btnRandom06.Text = "Random06";
+            this.btnRandom06.UseVisualStyleBackColor = true;
+            this.btnRandom06.Click += new System.EventHandler(this.btnRandom06_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(365, 296);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "label12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btnRandom06);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnRandom05);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRandom04);
+            this.Controls.Add(this.btnRandom03);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRandom02);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRandom01);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRandom01;
+        private System.Windows.Forms.Button btnRandom02;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRandom03;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRandom04;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnRandom05;
+        private System.Windows.Forms.Button btnRandom06;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Random Class/Random Class/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            label1.Text = rnd.Next().ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            label2.Text = rnd.Next(100).ToString();
+
+        }
+
+        private void btnRandom03_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            label3.Text = rnd.Next(90,100).ToString();
+        }
+
+        private void btnRandom04_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            label4.Text = rnd.Next(70, 100).ToString();
+            label5.Text = rnd.Next(70, 100).ToString();
+            label6.Text = rnd.Next(70, 100).ToString();
+            label7.Text = rnd.Next(70, 100).ToString();
+        }
+
+        private void btnRandom05_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            byte[] Arrys = new byte[5];
+            rnd.NextBytes(Arrys);
+            label8.Text = Arrys[0].ToString();
+            label9.Text = Arrys[1].ToString();
+            label10.Text = Arrys[2].ToString();
+            label11.Text = Arrys[3].ToString();
+        }
+
+        private void btnRandom06_Click(object sender, EventArgs e)
+        {
+            const string chars = "abcdefghıijklmnoöprsştuüvyzwqxABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string result = "";
+            Random rndm02 = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                result += chars[rndm02.Next(0, Convert.ToInt32(chars.Length))];
+            }
+            label12.Text = result;
+        }
+    }
+}
